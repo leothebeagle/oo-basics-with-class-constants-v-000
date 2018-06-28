bba6395e7c8a0e4da06f570e925f007435b5a6ff
Aziz Kosber <a.kosber@gmail.com>
2018-06-28 20:56:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,16 @@
 class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
+
   GENRES = []
 
   def initialize(title)
     @title = title
   end
 
-  def genre=(genre)
+  def genre= (genre)
     @genre = genre
-    GENRES << genre
+    GENRES << @genre
   end
 
   def turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,16 +3,15 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-
   def initialize(brand)
     @brand = brand
     BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+
   end
+
 
 
 end
